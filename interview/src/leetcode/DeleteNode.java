3c860f7e3871f499762db226e282799a54991e6a
wangyu <1321917368@qq.com>
2026-02-25 16:22:45 +0800

    删除链表中的节点

---
+++ interview/src/leetcode/DeleteNode.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class DeleteNode {
+    public static void main(String[] args) {
+
+    }
+
+    public static void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
