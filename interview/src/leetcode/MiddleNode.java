77752a6367ebfd7b88fa438f5af64aba11a365a9
wangyu <1321917368@qq.com>
2026-02-25 17:16:08 +0800

    leetcode876:链表中间节点

---
+++ interview/src/leetcode/MiddleNode.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class MiddleNode {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
