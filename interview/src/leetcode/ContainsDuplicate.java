1af5c5a7b2407a32c6767d1a9fe4b4b623b611b4
wangyu <1321917368@qq.com>
2026-03-04 17:05:30 +0800

    217:存在重复元素

---
+++ interview/src/leetcode/ContainsDuplicate.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            } else {
+                set.add(num);
+            }
+        }
+        return false;
+    }
+}
