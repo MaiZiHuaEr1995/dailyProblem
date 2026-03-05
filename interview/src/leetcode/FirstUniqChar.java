ac85ec12ddb0dbd282a28c8ee27059e6c858d0bd
wangyu <1321917368@qq.com>
2026-03-05 15:37:31 +0800

    leetcode：387 字符串中的第一个唯一字符串

---
+++ interview/src/leetcode/FirstUniqChar.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import java.util.HashMap;
+
+public class FirstUniqChar {
+    public static void main(String[] args) {
+
+    }
+
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char temp = s.charAt(i);
+            map.put(temp, map.getOrDefault(temp, 0) + 1);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char temp = s.charAt(i);
+            if (map.get(temp) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
